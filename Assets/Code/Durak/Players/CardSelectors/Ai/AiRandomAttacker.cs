cdbe4ab8be0a213f504c2b07bad4ffe3c80695f0
Кряжевских Никита <50074282+Kibastian@users.noreply.github.com>
2024-02-29 00:01:12 +0300

    Update AiRandomAttacker.cs

--- Assets/Code/Durak/Players/CardSelectors/Ai/AiRandomAttacker.cs
+++ Assets/Code/Durak/Players/CardSelectors/Ai/AiRandomAttacker.cs
@@ -8,6 +8,7 @@ using Framework.Shared.Collections;
 using Framework.Shared.Collections.Extensions;
 
 using UnityEngine;
+using UnityEngine.Experimental.Playables;
 
 namespace Framework.Durak.Players.Selectors
 {
@@ -22,6 +23,9 @@ namespace Framework.Durak.Players.Selectors
         private List<Data> attacking = new List<Data>();
         private List<Data> defending = new List<Data>();
         private List<IHand> hands = new List<IHand>();
+        private int passed = 0;
+        private int cur = 0;
+        private int totalpassed = 0;
 
         public AiRandomAttacker(IBoard<Data> board, IMap<ICard, Data> map, IDeck<Data> deck, IPlayerQueue<IPlayer> players)
         {
@@ -33,20 +37,41 @@ namespace Framework.Durak.Players.Selectors
         }
         private void TreeConstruct(int i, int who)
         {
+            if (Current.tree.collection.Count>5000)
+            {
+                Debug.Log("FUCK");
+            }
             Current.tree.Add((new Dictionary<Data, int>(), 0));
+            var tmp = passed;
             foreach (var e in hands[who])
             {
                 if (attacking.Contains(e) || defending.Contains(e)) continue;
                 if (Validate(who,e))
                 {
+                    
+                    passed = 0;
                     Current.tree.collection[i].Item1[e] = Current.tree.collection.Count;
-                    if (who == 0) attacking.Add(e);
+                    if (who < 2) attacking.Add(e);
                     else defending.Add(e);
-                    TreeConstruct(Current.tree.collection.Count, (who + 1) % 2);
-                    if (who == 0) attacking.Remove(e);
+                    TreeConstruct(Current.tree.collection.Count, (who<2)?2:cur);
+                    if (who < 2) attacking.Remove(e);
                     else defending.Remove(e);
                 }
             }
+            passed = tmp;
+            if (attacking.Count == 0) return;
+            if (who == 2) return;
+            passed++;
+            if (passed == 2)
+            {
+                passed--;
+                return;
+            }
+            Current.tree.collection[i].Item1[new Data()] = Current.tree.collection.Count;
+            cur = (cur + 1) % 2;
+            TreeConstruct(Current.tree.collection.Count, cur);
+            passed--;
+            cur = (cur + 1) % 2;
         }
         private bool ContainsRank(Data data)
         {
@@ -68,7 +93,7 @@ namespace Framework.Durak.Players.Selectors
         }
         private bool Validate(int who, Data data)
         {
-            if (who==0)
+            if (who<2)
             {
                 if (attacking.Count==0)
                 {
@@ -92,11 +117,14 @@ namespace Framework.Durak.Players.Selectors
         }
         public override ICard GetCard(IReadOnlyList<Data> hand)
         {
-            if (board.IsEmpty) first_t = true;
-            else first_t = false;
+            foreach (var e in board.Attacks)
+                attacking.Add(e);
+            foreach (var e in board.Defends)
+                defending.Add(e);
+            if (board.Count > 0) passed++;
             if (Current.tree.IsEmpty())
             {
-                hands = new List<IHand>(){ (IHand)(Current.Hand).Clone(), (IHand)defer.Clone(), (IHand)supper.Clone() };
+                hands = new List<IHand>(){ (IHand)(Current.Hand).Clone(),  (IHand)supper.Clone(), (IHand)defer.Clone() };
                 TreeConstruct(0, 0);
             }
             if (board.IsEmpty)
